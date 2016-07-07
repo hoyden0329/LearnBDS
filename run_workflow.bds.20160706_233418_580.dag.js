b8a1e0fc89041126bc328697caad3e1efd31a519
Xiu Huang <xiu.huang@aya.yale.edu>
2016-07-07 15:41:36 -0400

    Add files and update bds code.

---
+++ run_workflow.bds.20160706_233418_580.dag.js
@@ -0,0 +1,63 @@
+$(function(){ // on dom ready
+
+var cy = cytoscape({
+  container: document.getElementById('cy'),
+  
+  style: [
+    {
+      selector: 'node',
+      css: {
+        'content': 'data(id)',
+        'text-valign': 'center',
+        'text-halign': 'center'
+      }
+    },
+    {
+      selector: 'edge',
+      css: {
+        'target-arrow-shape': 'triangle'
+      }
+    },
+    {
+      selector: ':selected',
+      css: {
+        'background-color': 'red',
+        'line-color': 'red',
+        'target-arrow-color': 'red',
+        'source-arrow-color': 'red'
+      }
+    }
+  ],
+  
+  elements: {
+    nodes: [
+      { data: { id: 'thread_Root' } },
+      { data: { id: 'task.run_workflow.line_19.id_1', parent: 'thread_Root' } },
+      { data: { id: 'task.run_workflow.line_24.id_2', parent: 'thread_Root' } },
+      { data: { id: 'task.run_workflow.line_28.id_3', parent: 'thread_Root' } },
+      { data: { id: 'task.run_workflow.line_32.id_4', parent: 'thread_Root' } },
+    ],
+    edges: [
+      { data: { id: 'None-thread_Root', source: 'None', target: 'thread_Root' } },
+      { data: { id: 'threadid-threadid', source: 'threadid', target: 'threadid' } },
+      { data: { id: 'task.run_workflow.line_19.id_1-task.run_workflow.line_24.id_2', source: 'task.run_workflow.line_19.id_1', target: 'task.run_workflow.line_24.id_2' } },
+      { data: { id: 'task.run_workflow.line_19.id_1-task.run_workflow.line_24.id_2', source: 'task.run_workflow.line_19.id_1', target: 'task.run_workflow.line_24.id_2' } },
+      { data: { id: 'task.run_workflow.line_24.id_2-task.run_workflow.line_28.id_3', source: 'task.run_workflow.line_24.id_2', target: 'task.run_workflow.line_28.id_3' } },
+      { data: { id: 'task.run_workflow.line_24.id_2-task.run_workflow.line_28.id_3', source: 'task.run_workflow.line_24.id_2', target: 'task.run_workflow.line_28.id_3' } },
+      { data: { id: 'task.run_workflow.line_28.id_3-task.run_workflow.line_32.id_4', source: 'task.run_workflow.line_28.id_3', target: 'task.run_workflow.line_32.id_4' } },
+      { data: { id: 'task.run_workflow.line_24.id_2-task.run_workflow.line_32.id_4', source: 'task.run_workflow.line_24.id_2', target: 'task.run_workflow.line_32.id_4' } },
+      { data: { id: 'task.run_workflow.line_19.id_1-task.run_workflow.line_32.id_4', source: 'task.run_workflow.line_19.id_1', target: 'task.run_workflow.line_32.id_4' } },
+      { data: { id: 'task.run_workflow.line_28.id_3-task.run_workflow.line_32.id_4', source: 'task.run_workflow.line_28.id_3', target: 'task.run_workflow.line_32.id_4' } },
+      { data: { id: 'task.run_workflow.line_24.id_2-task.run_workflow.line_32.id_4', source: 'task.run_workflow.line_24.id_2', target: 'task.run_workflow.line_32.id_4' } },
+      { data: { id: 'task.run_workflow.line_19.id_1-task.run_workflow.line_32.id_4', source: 'task.run_workflow.line_19.id_1', target: 'task.run_workflow.line_32.id_4' } },
+      { data: { id: 'taskid-taskid', source: 'taskid', target: 'taskid' } },
+    ]
+  },
+  
+  layout: {
+    name: 'breadthfirst',
+  }
+});
+
+}); // on dom ready
+
